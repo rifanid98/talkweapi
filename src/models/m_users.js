fb3cd5f4f11871b04fa81f3c8c1d0d580761de95
rifanid98 <adninsijawa.office@gmail.com>
2020-07-27 01:35:03 +0700

    users model

---
+++ src/models/m_users.js
@@ -0,0 +1,137 @@
+/**
+ * Config MySQL
+ * .
+ * load config mysql
+ */
+const conn = require('../helpers/mysql');
+
+/**
+ * Helpers
+ */
+const myHelpers = require('../helpers/myHelpers');
+
+/**
+ * CRUD
+ */
+async function getData(filters, totalData, fields) {
+  const query = myHelpers.createQuery(filters, totalData, fields)
+  return new Promise((resolve, reject) => {
+    conn.query(query.sqlQuery, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+
+      const newResult = {
+        totalPage: query.totalPage,
+        result
+      }
+      result.map(data => delete data.password)
+      return Object.keys(filters.pagination).length > 0 ? resolve(newResult) : resolve(result);
+    })
+  })
+}
+
+function addData(data) {
+  return new Promise((resolve, reject) => {
+    const sqlQuery = "INSERT INTO users SET ?";
+    conn.query(sqlQuery, data, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
+    })
+  })
+}
+
+function updateData(data, id) {
+  return new Promise((resolve, reject) => {
+    const sqlQuery = "UPDATE users SET ? WHERE id = ?";
+    conn.query(sqlQuery, [data, id], function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
+    })
+  })
+}
+
+function deleteData(id) {
+  return new Promise((resolve, reject) => {
+    const sqlQuery = "DELETE FROM users WHERE id = ?";
+    conn.query(sqlQuery, id, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
+    })
+  })
+}
+
+/**
+ * Another CRUD
+ */
+function getDataById(id) {
+  return new Promise((resolve, reject) => {
+    const sqlQuery = "SELECT * FROM users WHERE id = ?";
+    conn.query(sqlQuery, id, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
+    })
+  })
+}
+
+function getDataByName(username) {
+  return new Promise((resolve, reject) => {
+    const sqlQuery = "SELECT * FROM users WHERE username = ?";
+    conn.query(sqlQuery, username, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
+    })
+  })
+}
+
+function getFieldsName() {
+  return new Promise((resolve, reject) => {
+    conn.query(`DESCRIBE users`, function (error, result) {
+      //      conn.query(`DESCRIBE users`, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      let fields = {};
+      result.forEach(field => {
+        fields[field.Field] = field.Field;
+      });
+      resolve(fields);
+    })
+  })
+
+}
+
+function getTotalData() {
+  return new Promise((resolve, reject) => {
+    conn.query(`SELECT COUNT(id) AS total_data FROM users`, function (error, result) {
+      //      conn.query(`DESCRIBE users`, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      resolve(result[0].total_data);
+    })
+  })
+
+}
+
+
+module.exports = {
+  getData,
+  addData,
+  updateData,
+  deleteData,
+  getDataById,
+  getDataByName,
+  getFieldsName,
+  getTotalData
+}
