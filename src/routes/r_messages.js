4da8db774ee422103b150f19742de5e186e60677
rifanid98 <adninsijawa.office@gmail.com>
2020-07-27 13:38:53 +0700

    messages route

---
+++ src/routes/r_messages.js
@@ -0,0 +1,37 @@
+/**
+ * ExpressJs FW
+ */
+// import express framework
+const express = require('express');
+// instance of express router
+const router = express.Router();
+
+// import multer upload helper
+const upload = require('../helpers/upload');
+
+/**
+ * Controllers
+ */
+// load the controller
+const messagesController = require('../controllers/c_messages');
+
+
+/**
+ * Main CRUD
+ */
+// Get All Messages
+router.get('/', messagesController.getMessages);
+// Post a Message
+router.post('/', upload.none(), messagesController.postMessage);
+// Patch a Message
+router.patch('/:id', upload.none(), messagesController.patchMessage);
+// Delete a Message
+router.delete('/:id', messagesController.deleteMessage);
+
+/**
+ * Ext CRUD
+ */
+// Get Message By ID
+router.get('/:id', messagesController.getMessageById);
+
+module.exports = router;
