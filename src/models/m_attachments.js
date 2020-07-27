ba61ab02baf3bd354e31dfecce21e9632c380d4a
rifanid98 <adninsijawa.office@gmail.com>
2020-07-27 13:38:03 +0700

    attachments model

---
+++ src/models/m_attachments.js
@@ -0,0 +1,124 @@
+/**
+ * Config MySQL
+ * .
+ * load config mysql
+ */
+const conn = require('../helpers/mysql');
+
+/**
+ * Helpers
+ */
+const myHelpers = require('../helpers/myHelpers');
+
+/**
+ * CRUD
+ */
+function getData(filters, totalData, fields) {
+  const sqlQuery = "SELECT * FROM attachments ";
+  const query = myHelpers.createQuery(sqlQuery, filters, totalData, fields)
+  return new Promise((resolve, reject) => {
+    conn.query(query.sqlQuery, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+
+      const newResult = {
+        totalPage: query.totalPage,
+        result
+      }
+      
+      return Object.keys(filters.pagination).length > 0 ? resolve(newResult) : resolve(result);
+    })
+  })
+}
+
+function addData(data) {
+  return new Promise((resolve, reject) => {
+    const sqlQuery = "INSERT INTO attachments SET ?";
+    conn.query(sqlQuery, data, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
+    })
+  })
+}
+
+function updateData(data, id) {
+  return new Promise((resolve, reject) => {
+    const sqlQuery = "UPDATE attachments SET ? WHERE id = ?";
+    conn.query(sqlQuery, [data, id], function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
+    })
+  })
+}
+
+function deleteData(id) {
+  return new Promise((resolve, reject) => {
+    const sqlQuery = "DELETE FROM attachments WHERE id = ?";
+    conn.query(sqlQuery, id, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
+    })
+  })
+}
+
+/**
+ * Another CRUD
+ */
+function getDataById(id) {
+  return new Promise((resolve, reject) => {
+    const sqlQuery = "SELECT * FROM attachments WHERE id = ?";
+    conn.query(sqlQuery, id, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
+    })
+  })
+}
+
+function getFieldsName() {
+  return new Promise((resolve, reject) => {
+    conn.query(`DESCRIBE attachments`, function (error, result) {
+      //      conn.query(`DESCRIBE attachments`, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      let fields = {};
+      result.forEach(field => {
+        fields[field.Field] = field.Field;
+      });
+      resolve(fields);
+    })
+  })
+
+}
+
+function getTotalData() {
+  return new Promise((resolve, reject) => {
+    conn.query(`SELECT COUNT(id) AS total_data FROM attachments`, function (error, result) {
+      //      conn.query(`DESCRIBE attachments`, function (error, result) {
+      if (error) {
+        reject(error);
+      }
+      resolve(result[0].total_data);
+    })
+  })
+
+}
+
+module.exports = {
+  getData,
+  addData,
+  updateData,
+  deleteData,
+  getDataById,
+  getFieldsName,
+  getTotalData,
+}
