079c6b1f202f44504d7bc5a2ed021449f6fb91fa
rifanid98 <adninsijawa.office@gmail.com>
2020-07-27 13:36:27 +0700

    add friends, messages & attachments route

--- src/routes/r_index.js
+++ src/routes/r_index.js
@@ -13,13 +13,19 @@ const validationMiddleware = require('../middlewares/mdl_validation');
 /**
  * Load All Routes
  */
-const usersRouter = require('./r_users');
 const authRouter = require('./r_auth');
+const usersRouter = require('./r_users');
+const friendsRouter = require('./r_friends');
+const messagesRouter = require('./r_messages');
+const attachmentsRouter = require('./r_attachments');
 
 /**
  * Fire the router
  */
-router.use('/users', validationMiddleware.xssEscape, authMiddleware.verifyJwtToken, usersRouter);
 router.use('/auth', validationMiddleware.xssEscape, authRouter);
+router.use('/users', validationMiddleware.xssEscape, authMiddleware.verifyJwtToken, usersRouter);
+router.use('/friends', validationMiddleware.xssEscape, authMiddleware.verifyJwtToken, friendsRouter);
+router.use('/messages', validationMiddleware.xssEscape, authMiddleware.verifyJwtToken, messagesRouter);
+router.use('/attachments', validationMiddleware.xssEscape, authMiddleware.verifyJwtToken, attachmentsRouter);
 
 module.exports = router;
