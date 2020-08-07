21b929ed4f83bff4e69ca59bc1609e6c8e41dd2d
rifanid98 <adninsijawa.office@gmail.com>
2020-08-07 19:17:55 +0700

    update database

--- talkwe.sql
+++ talkwe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 05 Agu 2020 pada 18.11
+-- Waktu pembuatan: 07 Agu 2020 pada 14.17
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.5
 
@@ -70,15 +70,18 @@ INSERT INTO `friends` (`id`, `user_id1`, `user_id2`, `status`, `created_at`, `up
 (85, 14, 4, 1, '2020-08-05 04:35:47', '2020-08-05 14:27:52'),
 (86, 5, 14, 1, '2020-08-05 04:36:02', '2020-08-05 04:36:02'),
 (87, 4, 14, 1, '2020-08-05 14:27:52', '2020-08-05 14:27:52'),
-(88, 4, 5, 0, '2020-08-05 14:36:31', '2020-08-05 14:36:31'),
+(88, 4, 5, 1, '2020-08-05 14:36:31', '2020-08-06 08:14:09'),
 (89, 4, 7, 1, '2020-08-05 14:36:35', '2020-08-05 14:37:04'),
 (90, 4, 8, 1, '2020-08-05 14:36:40', '2020-08-05 14:37:17'),
 (91, 4, 9, 1, '2020-08-05 14:36:44', '2020-08-05 14:37:50'),
 (92, 7, 4, 1, '2020-08-05 14:37:04', '2020-08-05 14:37:04'),
 (93, 8, 4, 1, '2020-08-05 14:37:17', '2020-08-05 14:37:17'),
 (94, 9, 4, 1, '2020-08-05 14:37:50', '2020-08-05 14:37:50'),
-(95, 15, 4, 0, '2020-08-05 14:51:51', '2020-08-05 14:51:51'),
-(96, 13, 4, 0, '2020-08-05 15:02:04', '2020-08-05 15:02:04');
+(96, 13, 4, 1, '2020-08-05 15:02:04', '2020-08-06 07:11:02'),
+(97, 17, 6, 1, '2020-08-06 03:39:54', '2020-08-06 03:40:28'),
+(98, 6, 17, 1, '2020-08-06 03:40:28', '2020-08-06 03:40:28'),
+(99, 4, 13, 1, '2020-08-06 07:11:02', '2020-08-06 07:11:02'),
+(100, 5, 4, 1, '2020-08-06 08:14:09', '2020-08-06 08:14:09');
 
 -- --------------------------------------------------------
 
@@ -112,15 +115,21 @@ INSERT INTO `messages` (`id`, `sender_id`, `receiver_id`, `message`, `message_re
 (233, 14, 5, 'Saya', 1, NULL, '2020-08-05 07:16:07', '2020-08-05 07:16:08'),
 (234, 14, 5, 'Hai', 1, NULL, '2020-08-05 07:16:29', '2020-08-05 07:16:29'),
 (235, 14, 5, 'Hai', 1, NULL, '2020-08-05 07:16:36', '2020-08-05 07:16:40'),
-(236, 8, 4, 'Good evening', 0, NULL, '2020-08-05 14:37:31', '2020-08-05 14:37:31'),
+(236, 8, 4, 'Good evening', 1, NULL, '2020-08-05 14:37:31', '2020-08-06 16:52:49'),
 (237, 9, 4, 'Are you okay sir? ', 1, NULL, '2020-08-05 14:38:19', '2020-08-05 14:39:22'),
-(238, 4, 6, 'Hei :D', 0, NULL, '2020-08-05 14:39:00', '2020-08-05 14:39:00'),
+(238, 4, 6, 'Hei :D', 1, NULL, '2020-08-05 14:39:00', '2020-08-06 02:17:24'),
 (239, 4, 7, 'We have some awesome projects last week...', 1, NULL, '2020-08-05 14:40:08', '2020-08-05 14:42:10'),
 (240, 7, 4, 'Yeah, of course', 1, NULL, '2020-08-05 14:42:34', '2020-08-05 14:43:48'),
 (241, 7, 4, 'Will there be more projects next week?', 1, NULL, '2020-08-05 14:43:39', '2020-08-05 14:43:48'),
 (242, 4, 7, 'Maybe yes', 1, NULL, '2020-08-05 14:44:23', '2020-08-05 14:44:24'),
 (243, 7, 4, 'What is the project about next week?', 1, NULL, '2020-08-05 14:45:25', '2020-08-05 14:45:26'),
-(244, 4, 7, 'Seems to be about a chat application', 1, NULL, '2020-08-05 14:46:16', '2020-08-05 14:46:17');
+(244, 4, 7, 'Seems to be about a chat application', 1, NULL, '2020-08-05 14:46:16', '2020-08-05 14:46:17'),
+(245, 4, 6, 'hai juga', 1, NULL, '2020-08-06 02:20:04', '2020-08-06 02:20:04'),
+(246, 6, 4, 'apa kabar', 1, NULL, '2020-08-06 02:20:12', '2020-08-06 02:20:12'),
+(247, 4, 6, 'baik, kamu?', 1, NULL, '2020-08-06 02:20:20', '2020-08-06 02:20:20'),
+(248, 17, 6, 'Hai', 1, NULL, '2020-08-06 03:41:11', '2020-08-06 03:41:24'),
+(249, 6, 17, 'Hai juga', 1, NULL, '2020-08-06 03:41:33', '2020-08-06 03:41:33'),
+(250, 17, 6, 'Apa kabar', 1, NULL, '2020-08-06 03:41:47', '2020-08-06 03:41:51');
 
 -- --------------------------------------------------------
 
@@ -136,6 +145,7 @@ CREATE TABLE `users` (
   `password` varchar(100) NOT NULL,
   `image` varchar(255) NOT NULL,
   `online` int(1) NOT NULL DEFAULT 0,
+  `status` varchar(150) NOT NULL DEFAULT 'Hi there! I''m using TalkWe',
   `location` varchar(255) DEFAULT '0,0',
   `location_share` int(1) NOT NULL DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -146,17 +156,17 @@ CREATE TABLE `users` (
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `full_name`, `email`, `password`, `image`, `online`, `location`, `location_share`, `created_at`, `updated_at`) VALUES
-(4, 'admin', 'admin manager', 'admin@talkwe.com', '$2b$10$GkG9XeGne3Te9JETETsZJ.n7.W1NMM9qK9vd31S3S.YA/PrU/uNXO', 'http://192.168.43.81:3000/talkwe/images/2020-08-05T14:34:03.844Z235544.jpg', 1, '-6.5851234,106.7184608', 1, '2020-07-26 17:57:13', '2020-08-05 15:01:27'),
-(5, 'staff', 'staff assistant', 'staff@talkwe.com', '$2b$10$O09TaCKwLysdXvvvvNu/meqmFK29SfsEsf7NPxjz.iEr1UNgIh9Pu', 'http://192.168.43.81:3000/talkwe/images/2020-08-05T14:33:14.838Z2422614.jpg', 1, '-6.5819333,106.720331', 1, '2020-07-26 18:56:06', '2020-08-05 14:33:25'),
-(6, 'user', 'user creative', 'user@talkwe.com', '$2b$10$5WjQp7KdQUBJniJWvg4Wuubu/YCTQwtwH5gSvYxtmbVjhj6cFDLTG', 'http://192.168.43.81:3000/talkwe/images/2020-08-05T13:51:52.008ZFB_IMG_15927595979202839.jpg', 1, '-6.5819333,106.720331', 1, '2020-07-26 18:56:22', '2020-08-05 14:33:47'),
-(7, 'adnin', 'adnin rifandi', 'adnin@talkwe.com', '$2b$10$UHmyTImdGcd0o3vdioJb1OJenZjJVRmfvsel3EV594QjDKIssOl06', 'http://192.168.43.81:3000/talkwe/images/2020-08-05T14:21:37.860Z235539.jpg', 0, '-6.5821431,106.7202369', 1, '2020-07-26 19:15:46', '2020-08-05 14:46:34'),
-(8, 'rifandi', 'rifandi sutanto', 'rifandi@talkwe.com', '$2b$10$CXPUglsQBuZetDJAD/pyBeHeEMy3.b2ZHQ.wec/8Nn.HyHcizuXcK', 'http://192.168.43.81:3000/talkwe/images/2020-08-05T14:22:15.787Zdownload.jpeg', 1, '-6.5819333,106.720331', 1, '2020-07-26 19:16:00', '2020-08-05 14:34:45'),
-(9, 'sutanto', 'sutanto putra', 'sutanto@talkwe.com', '$2b$10$jXNGLPD5NIIJFA9gILs1X.3xLe4ULQKGfrCoqPX08ja8kJfNJ/djy', 'http://192.168.43.81:3000/talkwe/images/2020-08-05T14:23:53.540Z2422630.jpg', 1, '-6.5853167,106.7186004', 1, '2020-07-26 19:16:19', '2020-08-05 14:37:47'),
-(10, 'putra', 'putra sanjaya', 'putra@talkwe.com', '$2b$10$GJQIWq14XdMlloyIMcjflOJUcUSTqqbUPUTXWDviLhLcpmujBt5y.', 'http://192.168.43.81:3000/talkwe/images/2020-08-05T14:24:27.531Z2422608.jpg', 1, '-6.5819333,106.720331', 1, '2020-07-26 19:16:28', '2020-08-05 14:35:19'),
-(13, 'gamen', 'gamen gamen', 'gamen@gamen.com', '$2b$10$ENv7u7MKftKNexKMtrkbcufJ/LYBJI/93rZKE.zsS6CGk7.hEJcMG', 'http://192.168.43.81:3000/talkwe/images/2020-08-05T14:25:53.454Z2422609.jpg', 1, '-6.5819333,106.720331', 1, '2020-07-30 04:13:52', '2020-08-05 15:51:10'),
-(14, 'talkwe', 'Talkwe Talkwe', 'talkwe@talkwe.com', '$2b$10$5rNbLyOuJ3Stqm3FGa8Xcua4ZxVPmbP/dlJaha8fNd.5VpG8RZ7Wy', 'http://192.168.43.81:3000/talkwe/images/2020-08-05T14:36:01.632Z2422630.jpg', 1, '-6.5819333,106.720331', 0, '2020-08-05 04:05:59', '2020-08-05 14:36:08'),
-(15, 'mark', 'Mark Zuckerberg', 'mark@facebook.com', '$2b$10$eaoLNMvlldxdpJNPdV4sTuY1/CdvwtHgtie.ZJJOZSNy0QBqLOidy', 'http://192.168.43.81:3000/talkwe/images/2020-08-05T14:49:22.031Z2422608.jpg', 1, '-6.5819333,106.720331', 0, '2020-08-05 14:48:41', '2020-08-05 14:49:23');
+INSERT INTO `users` (`id`, `username`, `full_name`, `email`, `password`, `image`, `online`, `status`, `location`, `location_share`, `created_at`, `updated_at`) VALUES
+(4, 'admin', 'admin manager', 'admin@talkwe.com', '$2b$10$GkG9XeGne3Te9JETETsZJ.n7.W1NMM9qK9vd31S3S.YA/PrU/uNXO', 'http://192.168.43.81:3000/talkwe/images/2020-08-06T16:11:14.110ZIMG-20200806-WA0002.jpg', 1, 'Hi there! I\'m using TalkWeeee', '-6.5825761,106.720331', 0, '2020-07-26 17:57:13', '2020-08-06 19:13:12'),
+(5, 'staff', 'staff assistant', 'staff@talkwe.com', '$2b$10$O09TaCKwLysdXvvvvNu/meqmFK29SfsEsf7NPxjz.iEr1UNgIh9Pu', 'http://192.168.43.81:3000/talkwe/images/2020-08-06T12:17:54.819Z235544.jpg', 0, 'Hi there! I\'m using TalkWe', '-6.5825761,106.720331', 0, '2020-07-26 18:56:06', '2020-08-06 13:45:40'),
+(6, 'user', 'user creative banget', 'user@talkwe.com', '$2b$10$5WjQp7KdQUBJniJWvg4Wuubu/YCTQwtwH5gSvYxtmbVjhj6cFDLTG', 'http://192.168.43.81:3000/talkwe/images/2020-08-06T12:18:19.018Z2422614.jpg', 0, 'Hi there! I\'m using TalkWe', '-6.5825761,106.720331', 1, '2020-07-26 18:56:22', '2020-08-06 13:45:40'),
+(7, 'adnin', 'adnin rifandi', 'adnin@talkwe.com', '$2b$10$UHmyTImdGcd0o3vdioJb1OJenZjJVRmfvsel3EV594QjDKIssOl06', 'http://192.168.43.81:3000/talkwe/images/2020-08-06T12:18:43.400Z2422609.jpg', 0, 'Hi there! I\'m using TalkWe', '-6.5825761,106.720331', 1, '2020-07-26 19:15:46', '2020-08-06 13:45:40'),
+(8, 'rifandi', 'rifandi sutanto', 'rifandi@talkwe.com', '$2b$10$CXPUglsQBuZetDJAD/pyBeHeEMy3.b2ZHQ.wec/8Nn.HyHcizuXcK', 'http://192.168.43.81:3000/talkwe/images/2020-08-06T12:19:25.669Z2422608.jpg', 0, 'Hi there! I\'m using TalkWe', '-6.5825761,106.720331', 1, '2020-07-26 19:16:00', '2020-08-06 13:45:40'),
+(9, 'sutanto', 'sutanto putra', 'sutanto@talkwe.com', '$2b$10$jXNGLPD5NIIJFA9gILs1X.3xLe4ULQKGfrCoqPX08ja8kJfNJ/djy', 'http://192.168.43.81:3000/talkwe/images/2020-08-06T12:19:51.539Z157244.jpg', 0, 'Hi there! I\'m using TalkWe', '-6.5825761,106.720331', 1, '2020-07-26 19:16:19', '2020-08-06 13:45:40'),
+(10, 'putra', 'putra sanjaya', 'putra@talkwe.com', '$2b$10$GJQIWq14XdMlloyIMcjflOJUcUSTqqbUPUTXWDviLhLcpmujBt5y.', 'http://192.168.43.81:3000/talkwe/images/2020-08-06T12:13:24.303Z2422630.jpg', 0, 'Hi there! I\'m using TalkWe', '-6.5825761,106.720331', 1, '2020-07-26 19:16:28', '2020-08-06 13:45:40'),
+(13, 'gamen', 'gamen gamen', 'gamen@gamen.com', '$2b$10$ENv7u7MKftKNexKMtrkbcufJ/LYBJI/93rZKE.zsS6CGk7.hEJcMG', 'http://192.168.43.81:3000/talkwe/images/2020-08-06T12:21:11.995ZFB_IMG_15927595979202839.jpg', 0, 'Hi there! I\'m using TalkWe', '-6.5825761,106.720331', 1, '2020-07-30 04:13:52', '2020-08-06 13:52:20'),
+(14, 'talkwe', 'Talkwe Talkwe', 'talkwe@talkwe.com', '$2b$10$5rNbLyOuJ3Stqm3FGa8Xcua4ZxVPmbP/dlJaha8fNd.5VpG8RZ7Wy', 'http://192.168.43.81:3000/talkwe/images/2020-08-06T12:20:13.820Z235539.jpg', 0, 'Hi there! I\'m using TalkWe', '-6.5825761,106.720331', 0, '2020-08-05 04:05:59', '2020-08-06 13:45:40'),
+(17, 'mark', 'mark Zuckerberg', 'mark@facebook.com', '$2b$10$OI60FdX6XYaOW9DxK1JUgOuiWONRmofUzQ2bMR6g/hX9shrGUzXZ.', 'http://192.168.43.81:3000/talkwe/images/2020-08-06T12:20:35.091Zdownload.jpeg', 0, 'Hi there! I\'m using TalkWe', '-6.5825761,106.720331', 0, '2020-08-06 03:33:38', '2020-08-06 13:45:40');
 
 -- --------------------------------------------------------
 
@@ -229,19 +239,19 @@ ALTER TABLE `attachments`
 -- AUTO_INCREMENT untuk tabel `friends`
 --
 ALTER TABLE `friends`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 
 --
 -- AUTO_INCREMENT untuk tabel `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=245;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=251;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
