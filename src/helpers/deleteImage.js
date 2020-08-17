4b5244f51bb074696b20e8529422e14c37aad953
rifanid98 <adninsijawa.office@gmail.com>
2020-08-17 19:33:37 +0700

    fix bugs

--- src/helpers/deleteImage.js
+++ src/helpers/deleteImage.js
@@ -6,10 +6,12 @@ const config = require("../configs/global");
 
 module.exports = {
   delete: function (myRequest, file_name = "") {
-    const newFileName = file_name.split(`${myRequest.protocol}://${myRequest.host}/${config.rootProjectPath}/images/`).pop();
-    const targetFile = `${imagePath}${newFileName}`;
+    // const newFileName = file_name.split(`${myRequest.protocol}://${myRequest.host}/${config.rootProjectPath}/images/`).pop();
+    // const targetFile = `${imagePath}${newFileName}`;
+    const targetFile = `${imagePath}${file_name}`;
 
-    if (newFileName.split(`.`)[0] !== `default` || newFileName.split(`.`)[0] !== `avatar`) {
+    // if (newFileName.split(`.`)[0] !== `default` || newFileName.split(`.`)[0] !== `avatar`) {
+    if (file_name.split(`.`)[0] !== `default` || file_name.split(`.`)[0] !== `avatar`) {
       if (fs.existsSync(targetFile)) {
         try {
           fs.unlinkSync(targetFile);
